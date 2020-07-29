b9c539d23c24422bda7b93b2751bd6d22c8708f3
Robert De la Rosa <rdelarosa39@gmail.com>
2020-07-29 00:02:46 -0500

    exercise 11 refactored

--- code.js
+++ code.js
@@ -8,34 +8,5 @@ function sayHello(arg){
 }
 
 function isEven(arg) {
-    if(arg === 2){
-        return true;
-    }
-    else if(arg === -4){
-        return true;
-    }
-    else if(arg === 3){
-        return false;
-    }
-    else if(arg === "banana"){
-        return false;
-    }
-    else if(arg === "8"){
-        return true;
-    }
-    else if(arg === Infinity){
-        return false;
-    }
-    else if(arg === true){
-        return false;
-    }
-    else if(arg === false){
-        return false;
-    }
-    else if(arg === undefined){
-        return false;
-    }
-    else{
-        return true;
-    }
+    return parseInt(arg)%2===0;
 }
